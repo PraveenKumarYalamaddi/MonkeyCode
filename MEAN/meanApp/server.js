3cb8c3a7c784a97d9f2ea8f15ae7a03b55976f17
Praveen Kumar Yalamaddi <praveen.kumar@pervacio.com>
2018-04-08 01:33:08 +0530

    port changed

--- MEAN/meanApp/server.js
+++ MEAN/meanApp/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const api= ('./server/routes/api');
-const port =3000;
+const port =4000;
 const app = express();
 app.use(express.static(path.join(__dirname,'dist')));
 app.use(bodyParser.urlencoded({extended:true}));
