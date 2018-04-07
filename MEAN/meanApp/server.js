9f09fd18baa2a0ef248c174912db3d722e283f50
Praveen Kumar Yalamaddi <praveen.kumar@pervacio.com>
2018-04-08 01:50:33 +0530

    resolved TypeError: Router.use() requires a middleware function but got a string

--- MEAN/meanApp/server.js
+++ MEAN/meanApp/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(path.join(__dirname,'dist')));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-app.use('/api' ,api);
+app.use('/api' ,require(api));
 app.get('*',(req,res)=>{res.sendfile(path.json(__dirname,'dist/index.html'))});
 app.listen(port,function(){
     console.log('Server running on port:' +port);
